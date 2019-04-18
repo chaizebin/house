56ee101279440e4df2dbdd22951f9eb97c335ac8
zanjubei <1374346010@qq.com>
2019-04-18 19:19:23 +0800

    wxy

---
+++ hetong_provider_wxy/src/main/java/com/jk/HetongProviderWxyApplication.java
@@ -0,0 +1,13 @@
+package com.jk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HetongProviderWxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HetongProviderWxyApplication.class, args);
+    }
+
+}
