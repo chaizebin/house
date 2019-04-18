708121f3fa0aac35b25d913d8fae54effc9e7a87
yuanshuoshuo <987177067@qq.com>
2019-04-18 19:50:33 +0800

    å查询前台

---
+++ fangyuan-feign-ys/src/main/java/com/jk/controller/HouseController.java
@@ -0,0 +1,56 @@
+package com.jk.controller;
+
+import com.jk.model.HouseBean;
+import com.jk.service.HouseServiceFeign;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+/**
+ * @program: springcloud_house
+ * @Date: 2019/4/17 8:42
+ * @Author: 袁硕
+ * @Description:
+ */
+@Controller
+@RequestMapping("houseFeign")
+public class HouseController {
+
+    @Autowired
+    private HouseServiceFeign houseServiceFeign;
+
+    @GetMapping("selectHousePage")
+    @ResponseBody
+    public Map<String,Object> selectHousePage(Integer page , Integer rows){
+        System.out.println(page+"--"+rows);
+        return houseServiceFeign.selectHousePage(page,rows);
+    }
+
+    @DeleteMapping("deleteHouseById")
+    @ResponseBody
+    public void deleteHouseById(Integer id){
+        System.out.println(id);
+        houseServiceFeign.deleteHouseById(id);
+    }
+    @DeleteMapping("delAllHouse")
+    @ResponseBody
+    public void delAllHouse(String ss){
+        System.out.println("删除id:--"+ss);
+        houseServiceFeign.delAllHouse(ss);
+    }
+
+    @GetMapping("selectHouseById")
+    @ResponseBody
+    public HouseBean selectHouseById(Integer id){
+        System.out.println(id);
+        return houseServiceFeign.selectHouseById(id);
+    }
+    @PutMapping("updateHouse")
+    @ResponseBody
+    public  void updateHouse(@RequestBody HouseBean houseBean){
+        System.out.println(houseBean);
+        houseServiceFeign.updateHouse(houseBean);
+    }
+}
---
+++ fangyuan-feign-ys/src/main/java/com/jk/service/HouseServiceFeign.java
@@ -0,0 +1,35 @@
+package com.jk.service;
+
+import com.jk.model.HouseBean;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+/**
+ * @program: springcloud_house
+ * @Date: 2019/4/17 8:46
+ * @Author: 袁硕
+ * @Description:
+ */
+@FeignClient("house-provider")
+public interface HouseServiceFeign {
+
+    @GetMapping("selectHousePage")
+    Map<String, Object> selectHousePage(@RequestParam("page") Integer page, @RequestParam("rows") Integer rows);
+
+    @DeleteMapping("deleteHouseById")
+    void deleteHouseById(@RequestParam("id") Integer id);
+
+    @DeleteMapping("delAllHouse")
+    void delAllHouse(@RequestParam("ss") String ss);
+
+    @GetMapping("selectHouseById")
+    HouseBean selectHouseById(@RequestParam("id") Integer id);
+
+    @PutMapping("updateHouse")
+    void updateHouse(HouseBean houseBean);
+}
