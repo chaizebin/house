c96ff654b36352bdf72e21d6fabb9313f0f1695b
yuanshuoshuo <987177067@qq.com>
2019-04-18 19:17:26 +0800

    查询和删除

---
+++ common/src/main/java/com/jk/model/HouseBean.java
@@ -0,0 +1,10 @@
+package com.jk.model;
+
+/**
+ * @program: house
+ * @Date: 2019/4/18 19:09
+ * @Author: 袁硕
+ * @Description:
+ */
+public class HouseBean {
+}
---
+++ fangyuan-provider-ys/src/main/java/com/jk/service/HouseService.java
@@ -0,0 +1,24 @@
+package com.jk.service;
+
+import com.jk.model.HouseBean;
+
+import java.util.Map;
+
+/**
+ * @program: springcloud_house
+ * @Date: 2019/4/16 17:19
+ * @Author: 袁硕
+ * @Description:
+ */
+public interface HouseService {
+
+    Map<String, Object> selectHousePage(Integer page, Integer rows);
+
+    void deleteHouseById(Integer id);
+
+    void delAllHouse(String ss);
+
+    HouseBean selectHouseById(Integer id);
+
+    void updateHouse(HouseBean houseBean);
+}
